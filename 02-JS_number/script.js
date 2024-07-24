ff49c7321ba37033638571b1a033074edb99f91a
Arief <mrarief0110@gmail.com>
2024-07-24 22:25:53 +0530

    learning about number system and their type

---
+++ 02-JS_number/script.js
@@ -0,0 +1,20 @@
+//  NUMBER
+// float,ineger, infinity
+let age = 20;
+let height = 5.15;
+let weight = 80;
+let slary = 6000;
+let zeroDecimal = 0.00103910231;
+let zero = 0;
+let one = 1;
+
+console.log(typeof age); // number
+console.log(typeof height); // number
+console.log(typeof weight); // number
+console.log(typeof slary); // number
+console.log(typeof zero); // number
+console.log(typeof zeroDecimal); // number
+console.log(typeof (zero/one)); // number /*---->infinity<-----*/
+
+/*---->not a number<-----*/
+console.log(typeof NaN); // number
