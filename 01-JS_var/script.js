710306022a2ee95613ef2e1256b517930ca5ce7d
Arief <mrarief0110@gmail.com>
2024-07-24 21:45:42 +0530

    learning on variables like var, let, const

---
+++ 01-JS_var/script.js
@@ -0,0 +1,16 @@
+// variables
+// --> decleration
+// --> initialization
+
+/*  1. Error */
+console.log(name);
+// var name = "arief"; --> undefiend
+// let name = "arief"; --> cannot acces name before initialization
+// const name = "arief"; --> cannot acces name before initialization
+
+/* 2. error */
+// var age
+// let age -->undefiend
+// const age -->undefiend
+
+console.log(age) 
